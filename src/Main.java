8c677bf4fa0b1afef5e68d45ac7d06488bb893bb
EnrickaM <marshallenricka@yahoo.com>
2019-05-13 16:09:06 -0500

    classwork

---
+++ src/Main.java
@@ -0,0 +1,72 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args){
+//        Problem 1
+        Scanner reader= new Scanner(System.in);
+//        System.out.println("Its a great Day");
+//        System.out.println("What's your name?");
+//        String name= reader.nextLine();
+//        System.out.println("Hello " + name);
+
+//        Problem 2
+//        System.out.println("Enter a number");
+//        System.out.println("Enter another number");
+//        int firstnumber=reader.nextInt();
+//        int secondnumber=reader.nextInt();
+//        System.out.println(firstnumber + secondnumber);
+//        System.out.println(firstnumber - secondnumber);
+
+//        Problem 3
+//        System.out.println("Enter your balance");
+//        int balacne=reader.nextInt();
+//        int freemoney=reader.nextInt();
+//        System.out.println("Ive granted you $1.50 "+balacne + freemoney);
+
+//        Problem 4
+//        System.out.println("Enter a password");
+//        System.out.println("Re-enter your password");
+//        String password1=reader.next();
+//        String password2=reader.next();
+//        if (password1.equals(password2)){
+//            System.out.println("Passwords match");}
+//        else{
+//            System.out.println("Passwords didnt match!!!");
+//        }
+
+//       Problem 5
+//        System.out.println("Enter nuber 1-3");
+//        int number1= reader.nextInt();
+//            if(number1==1 || number1==2 || number1 ==3){
+//            System.out.println("Correct");}
+//        else{
+//            System.out.println("Error");
+//            }
+
+//        problem 6
+        System.out.println("Enter A number");
+        int number=reader.nextInt();
+        if (number != 1){
+            System.out.println("Negative");}
+        else{
+            System.out.println("Postive");
+        }
+
+//        Problem 6
+        System.out.println("Enter 3 card numbers");
+        int card1=reader.nextInt();
+        int card2=reader.nextInt();
+        int card3=reader.nextInt();
+        int sum= card1 + card2 + card3;
+
+        if (card1 && card2 == 21 ||  )
+        }
+
+
+
+
+
+    }
+
+
